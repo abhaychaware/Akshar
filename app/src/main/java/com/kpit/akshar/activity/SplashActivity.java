46f026c730db5ae9ffeb06ac85505c3d948fd361
abhaychaware <abhay.chaware@gmail.com>
2016-01-12 13:43:56 +0530

    Base upgrade and UI enhancements
    Base upgrade and UI enhancements

---
+++ app/src/main/java/com/kpit/akshar/activity/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.kpit.akshar.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.kpit.akshar.R;
+
+/**
+ * Created by kpittechnologies on 1/8/16.
+ */
+public class SplashActivity extends AppCompatActivity {
+    private static final long SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashActivity.this,
+                        MainActivity.class);
+                Bundle bundle = ActivityOptionsCompat.makeCustomAnimation(
+                        SplashActivity.this, R.anim.in_right_animation,
+                        R.anim.out_left_animation).toBundle();
+                ActivityCompat.startActivity(SplashActivity.this, i, bundle);
+
+
+//                Intent i = new Intent(SplashActivity.this,
+//                        MainActivity.class);
+//                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+
+}
