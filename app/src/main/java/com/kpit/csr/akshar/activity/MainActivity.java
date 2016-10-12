c2fb904bd0a893d65f0fb5c872e6d5ad5f146deb
abhaychaware <abhay.chaware@gmail.com>
2016-10-12 08:52:01 +0530

    Added Madia audio, added credit activity

--- app/src/main/java/com/kpit/csr/akshar/activity/MainActivity.java
+++ app/src/main/java/com/kpit/csr/akshar/activity/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -27,7 +29,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private static final int TIME_INTERVAL = 2000;
     private long mBackPressed;
 
-    //Hemalkasa alphabet arrays
+    //static final String[] marathiAlphabets = {"m1", "m2", "m3", "m4", "m5", "m6", "m7", "m9", "m11", "l1", "l3", "s1", "l5", "l7", "l9", "l11", "l14", "l16", "l18", "l19", "l21", "l23", "l24", "l25", "l26", "l27", "l30", "l31", "s2", "l32"};
+    //Madia alphabet arrays
     static final String[] alphabets = {"m1", "m2", "m3", "m4", "m5", "m6", "m7", "m9", "m11", "l1", "l3", "s1", "l5", "l7", "l9", "l11", "l14", "l16", "l18", "l19", "l21", "l23", "l24", "l25", "l26", "l27", "l30", "l31", "s2", "l32"};
     static final String[] pics = {"pm1", "pm2", "pm3", "pm4", "pm5", "pm6", "pm7", "pm9", "pm11", "pl1", "pl3", "ps1", "pl5", "pl7", "pl9", "pl11", "pl14", "pl16", "pl18", "pl19", "pl21", "pl23", "pl24", "pl25", "pl26", "pl27", "pl30", "pl31", "ps2", "pl32"};
     static final String[] words = {"अनम", "आपा", "इल्‌वि", "ईचळ मीन", "उलि", "ऊज", "एर्‌रे", "ओडा", "अंज", "कय", "गब्‌ले", "मोलाङ", "चहा", "जगा", "टडो", "डाक्‌टर", "तला", "दळिया", "नय", "पल्‌क", "बरे", "मरा", "यायाल", "रय्‌के", "लसुस्‌क", "वग़्‌किङ", "सगम", "हामुर", "नेग़ल", "ताळ"};
@@ -92,10 +95,37 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             Toast.makeText(getBaseContext(), "धन्यवाद !!!",
                     Toast.LENGTH_SHORT).show();
             return;
-        } else {
-
-        }
+        } /*else {
+            //do nothing
+        }*/
 
         mBackPressed = System.currentTimeMillis();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.credits:
+                Intent i = new Intent(MainActivity.this,
+                        CreditsActivity.class);
+                Bundle bundle = ActivityOptionsCompat.makeCustomAnimation(
+                        MainActivity.this, R.anim.in_right_animation,
+                        R.anim.out_left_animation).toBundle();
+                ActivityCompat.startActivity(MainActivity.this, i, bundle);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
